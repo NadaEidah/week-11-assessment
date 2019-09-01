68fb2dc4b7aa2ce4ac3f7997544550c625a333cf
NadaEidah <44302867+NadaEidah@users.noreply.github.com>
2019-09-01 11:01:44 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,19 +4,36 @@
 //////////////////////// Your Code Here /////////////////////
 
 
+$emploee="Mr A";
+$salaries=200;
+$emploee2="Mr b";
+$salaries2=300;
+
+echo "salaries for " . $emploee . "is". $salaries ;
+echo "salaries for " . $emploee2 . "is". $salaries2 ;
+
+
 
 
 
 /*Question 2 : Write a PHP script which displays the capital and country name from the below array $c. as unordered list after sorting the array list by countries names  */
 //////////////////////// Your Code Here /////////////////////
 
-$countries = array( "Italy"=>"Rome",
+$countries = array( 
+                "Italy"=>"Rome",
               "Luxembourg"=>"Luxembourg",
                "Belgium"=> "Brussels",
                "Denmark"=>"Copenhagen",
                "Finland"=>"Helsinki",
                "France" => "Paris",
                "Slovakia"=>"Bratislava" ) ;
+
+echo "<ul>"
+for ($i=0 ; $countries<0 ; $i++) {
+    echo "city".$countries[key]."Capital".$countries[value]
+}
+
+echo "</ul>"
 
 /*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
 
@@ -32,10 +49,46 @@ $countries = array( "Italy"=>"Rome",
 *
 
 */
+$count=""
+for ($count=0; $count<0 ; $count++){
+    echo "*"
+    if($count<=5){
+        $count--
+        echo "*"
+    }
+    
+}
 
 /*Question 4 : Write a PHP script to calculate the difference between two dates.
 Input : 1981-11-04, 2013-09-04
-Output : 31 years, 10 months, 11 days
+Output : 31 years, 10 months, 11 days*/
 
+$d1=<input name="day">;
+$d2=<input name="day">;
+ 
+$out=($d1-$d2)
 
+echo $out ;
 /*Question 5 : Create a simple HTML form that accept the user name after submitting display the name using PHP echo statement under the form. */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>q5</title>
+</head>
+<body>
+    <?php
+
+        <form>
+        $name = <input type="text" name="name" >;
+        <input type="submit" value="Submit">
+        </form> 
+   
+        echo  $name ;
+    ?>
+</body>
+</html>
+
+?>
